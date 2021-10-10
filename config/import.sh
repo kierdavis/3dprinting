de06be7aa0effc6f7406619b4cd7b9a51842a9ae
Kier Davis <me@kierdavis.com>
2021-10-10 15:34:36 +0100

    fix import script

--- config/import.sh
+++ config/import.sh
@@ -1,6 +1,12 @@
-#!/bin/bash
+#!/bin/sh
 set -o errexit -o nounset -o pipefail
 
+link() {
+  if [[ -d "$2" ]]; then rm -rf "$2"; fi
+  mkdir -p "$(dirname "$2")"
+  ln -fsTv "$1" "$2"
+}
+
 here="$(dirname "$(readlink -f "$0")")"
-ln -fsTv "$here/repetier" "$HOME/.mono/registry/CurrentUser/software/repetier"
-ln -fsTv "$here/prusa-slicer" "$HOME/.config/PrusaSlicer"
+link "$here/repetier" "$HOME/.mono/registry/CurrentUser/software/repetier"
+link "$here/prusa-slicer" "$HOME/.config/PrusaSlicer"
